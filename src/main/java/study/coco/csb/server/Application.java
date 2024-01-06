30aa8900a9092e19c6aa5fb632876a2388a32675
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-06 22:22:55 +0100

    Add Spring Boot annotations and execution command
    We add the starting command for the Spring Boot Application to our `main` method.
    Additionally, we add the annotation `SpringBootApplication` to our `Application` class.
    
    We can now start the application and visit (http://localhost:8080) in the browser.
    
    Annotations are additional information that can be glued to certain elements
    of your Java program.
    
    This information can be used:
    - Information for the compiler - Annotations can be used by the compiler to detect errors or suppress warnings.
    - Compile-time and deployment-time processing - Software tools can process annotation information to generate code, XML files, and so forth.
    - Runtime processing - Some annotations are available to be examined at runtime.
    Source: https://docs.oracle.com/javase/tutorial/java/annotations/index.html
    
    We will see all three use cases during the lecture.
    
    Additional Resources:
    - https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started.html#getting-started.first-application.code
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/Application.java
+++ src/main/java/study/coco/csb/server/Application.java
@@ -1,9 +1,16 @@
 package study.coco.csb.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+
+    SpringApplication.run(Application.class, args);
+    // Goto http://localhost:8080/
   }
 
 }
