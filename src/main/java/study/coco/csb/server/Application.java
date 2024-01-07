4bb1d1b25ef59b97ae690848031ad19bee7abb78
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-07 23:04:03 +0100

    Code Cleanup
    Remove unused imports
    
    **tags**: #NotRelevantForExam

--- src/main/java/study/coco/csb/server/Application.java
+++ src/main/java/study/coco/csb/server/Application.java
@@ -2,13 +2,6 @@ package study.coco.csb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Application {
