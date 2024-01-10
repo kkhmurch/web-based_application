41bc6a287e30039f321dfe8a9a66f170629ff9cf
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-10 22:02:32 +0100

    Add CORS configuration to allow cross origin AJAX  requests
    We enable CORS for our server for all origins and all methods.
    
    Additional resources:
    - https://developer.mozilla.org/de/docs/Web/HTTP/CORS
    
    **tags**: #NotRelevantForExam

--- src/main/java/study/coco/csb/server/Application.java
+++ src/main/java/study/coco/csb/server/Application.java
@@ -2,6 +2,9 @@ package study.coco.csb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -11,6 +14,18 @@ public class Application {
 
     SpringApplication.run(Application.class, args);
     // Goto http://localhost:8080/
+  }
+
+  // https://spring.io/guides/gs/rest-service-cors/
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*");
+      }
+    };
   }
 
 }
