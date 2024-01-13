8a05ceb756a481ac337ce68e4a394e4c8881f601
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-13 20:39:32 +0100

    Use ReviewRepository instead of in-memory ArrayList  data structure
    The ReviewRepository offers equal methods to create, read and delete Reviews with the database:
    * reviewRepository.save()
    * reviewRepository.findAll()
    * reviewRepository.findById(id).get()
      * or reviewRepository.findById(id).orElse(null)
    * reviewRepository.deleteById(id);
    
    Only the update method has a special implementation.
    We first get the current state of the data before the update, then we override only comment and rating, and after that we save the changed state of the data.
    
    First this has business logic reasons, we want to make sure that the submittedAt date stays the same and cannot be changed.
    
    Second it has something to do with the persistence context of an entity.
    If the Review object is sent by the client, the object is not in the persistence context.
    Therefore, we have to get the real Review entity from the persistence context in order to change some data.
    
    !Import!
    
    The counter of our ids starts now at 1.
    
    Additional Resources:
    * https://www.baeldung.com/spring-data-crud-repository-save#updateInstance
    * https://www.baeldung.com/hibernate-save-persist-update-merge-saveorupdate#2-states-of-entity-instances
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/ReviewService.java
+++ src/main/java/study/coco/csb/server/ReviewService.java
@@ -3,7 +3,6 @@ package study.coco.csb.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,8 +10,6 @@ import java.util.List;
  */
 @Service
 public class ReviewService {
-
-  List<Review> reviews = new ArrayList<>();
 
   private ReviewRepository reviewRepository;
 
@@ -23,44 +20,37 @@ public class ReviewService {
   }
 
   private void initReviews() {
-    reviews.add(new Review("All good!", 4));
-    reviews.add(new Review("First class service and excellent product.", 5));
-    reviews.add(new Review("Bad communication and quite a delay before dispatch.", 1));
+    reviewRepository.save(new Review("All good!", 4));
+    reviewRepository.save(new Review("First class service and excellent product.", 5));
+    reviewRepository.save(new Review("Bad communication and quite a delay before dispatch.", 1));
   }
 
   public List<Review> getReviews() {
-    return reviews;
+    return reviewRepository.findAll();
   }
 
   public Review getReview(int index) {
-    Review result = null;
-    for (Review review : reviews) {
-      if (review.getId() == index) {
-        result = review;
-        break;
-      }
-    }
-    return result;
+    return reviewRepository.findById((long) index).orElse(null);
   }
 
-  public void submitReview(String comment) {
-    reviews.add(new Review (comment, 5));
+  public void submitReview(String comment, int rating) {
+    reviewRepository.save(new Review (comment, rating));
   }
 
   public void submitReview(Review review) {
-    reviews.add(review);
+    reviewRepository.save(review);
   }
 
   public Review updateReview(int index, String comment, int rating) {
     Review review = getReview(index);
     review.setComment(comment);
     review.setRating(rating);
-    return reviews.get(index);
+    return reviewRepository.save(review);
   }
 
   public Review deleteReview(int index) {
-    Review reviewToDelete = getReview(index);
-    reviews.remove(reviewToDelete);
+    Review reviewToDelete = reviewRepository.findById((long) index).get();
+    reviewRepository.deleteById((long) index);
     return reviewToDelete;
   }
 
