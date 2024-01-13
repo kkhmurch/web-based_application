a3d30124c4d0af71cf01e7a0050ddfc97a391ead
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-13 16:32:35 +0100

    Move business logic from UserController to  UserService
    We move the implementation of our business logic from the UserController to the UserService.
    
    We create the methods in the UserService and replace them in the UserController with a method call to the UserService.
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/UserController.java
+++ src/main/java/study/coco/csb/server/UserController.java
@@ -2,7 +2,6 @@ package study.coco.csb.server;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -10,33 +9,27 @@ public class UserController {
 
   UserService userService;
 
-  private ArrayList<String> users = new ArrayList<>();
-
   public UserController(UserService userService) {
     this.userService = userService;
-
-    users.add("Max Mustermann");
-    users.add("Erika Mustermann");
-    users.add("John Doe");
   }
 
   @GetMapping("/users")
   public List<String> getUsers() {
-    return users;
+    return userService.getUsers();
   }
 
   @GetMapping("/users/{id}")
   public String getUser(@PathVariable("id") int id) {
-    return users.get(id);
+    return userService.getUser(id);
   }
 
   @PostMapping("/users/submit")
   public void registerUser(@RequestParam("name") String name, @RequestParam("email") String email) {
-    users.add(name);
+    userService.createUser(name,email);
   }
 
   @DeleteMapping("/users/{id}")
   public String deleteUser(@PathVariable("id") int id) {
-    return users.remove(id);
+    return userService.deleteUser(id);
   }
 }
--- src/main/java/study/coco/csb/server/UserService.java
+++ src/main/java/study/coco/csb/server/UserService.java
@@ -2,6 +2,33 @@ package study.coco.csb.server;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserService {
+
+  private ArrayList<String> users = new ArrayList<>();
+
+  public UserService() {
+    users.add("Max Mustermann");
+    users.add("Erika Mustermann");
+    users.add("John Doe");
+  }
+
+  public List<String> getUsers() {
+    return users;
+  }
+
+  public String getUser(int id) {
+    return users.get(id);
+  }
+
+  public String deleteUser(int id) {
+    return users.remove(id);
+  }
+
+  public void createUser(String name, String email) {
+    users.add(name);
+  }
 }
