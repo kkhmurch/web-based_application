26dec9bc7fee943116f61eb83aa1704a637c4177
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-13 16:03:20 +0100

    Add getUsers Java method
    We add a simple Java method that will return all users of the application.
    
    At the moment we will just return an empty List of Strings.
    It is just enough code, so that the Class can be complied without errors.
    
    If you start the application, it will have no new documentation under (http://localhost:8080/swagger-ui.html); as there is still missing the @RequestMapping annotation.
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/UserController.java
+++ src/main/java/study/coco/csb/server/UserController.java
@@ -2,7 +2,13 @@ package study.coco.csb.server;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
 
+  public List<String> getUsers() {
+    return new ArrayList<String>();
+  }
 }
