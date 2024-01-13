3a2f5faeda61373d4b8042906c64198b37f3d161
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-13 16:05:42 +0100

    Add GET RequestMapping to getUsers method
    We add the @GetMapping annotation to the method.
    
    This will register the path "/users" for the HTTP method "GET" to the getUser method in the application.
    
    Now HTTP GET requests (http://localhost:8080/users) will return a successful HTTP response.
    At the moment only an empty list `[]` is returned, because our Java method returns only an empty ArrayList.
    
    Before this change, an HTTP GET request to (http://localhost:8080/users) returned a client error (type= Not Found, status= 404).
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/UserController.java
+++ src/main/java/study/coco/csb/server/UserController.java
@@ -1,5 +1,6 @@
 package study.coco.csb.server;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 @RestController
 public class UserController {
 
+  @GetMapping("/users")
   public List<String> getUsers() {
     return new ArrayList<String>();
   }
