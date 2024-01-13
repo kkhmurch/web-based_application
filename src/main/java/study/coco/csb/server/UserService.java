2775639b02085471b99c423c543c6b08329ee2ed
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-13 16:37:20 +0100

    Change internal UserService implementation to use  User class instead of String.
    We change the internal UserService implementation to use an ArrayList of User instead an ArrayList of String to store our User data.
    
    This is only an intermediate step to replace the former String User data with the User class.
    
    Advanced stream usage to transform the ArrayList users into the old data structure of ArrayList of Strings for the getUsers method. #NotRelevantForExam
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/UserService.java
+++ src/main/java/study/coco/csb/server/UserService.java
@@ -4,31 +4,32 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
-  private ArrayList<String> users = new ArrayList<>();
+  private ArrayList<User> users = new ArrayList<>();
 
   public UserService() {
-    users.add("Max Mustermann");
-    users.add("Erika Mustermann");
-    users.add("John Doe");
+    users.add(new User("Max Mustermann", "max.mustermann@example.com"));
+    users.add(new User("Erika Mustermann", "erika.mustermann@example.com"));
+    users.add(new User("John Doe", "john.doe@example.com"));
   }
 
   public List<String> getUsers() {
-    return users;
+    return users.stream().map(User::getName).collect(Collectors.toList());
   }
 
   public String getUser(int id) {
-    return users.get(id);
+    return users.get(id).getName();
   }
 
   public String deleteUser(int id) {
-    return users.remove(id);
+    return users.remove(id).getName();
   }
 
   public void createUser(String name, String email) {
-    users.add(name);
+    users.add(new User(name, email));
   }
 }
