22ae0b681ffc87bd02e8521f8036054dca1fe4a9
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-15 21:05:00 +0100

    Add JPA annotation for User
    * Add @Entity annotation for the class.
    * Add @Table annotation for the class, because of the reserved keyword USER in SQL.
    * Add field id of type Long
    * Add @Id annotation to field id
    * Add @GeneratedValue annotation to field, so that the database will take care of maintaining the id counter.
    * Add protected default constructor for ORM
    * Add getId() method for mapping to JSON object
    
    Because there are reserved keywords in SQL we cannot create a table with the name "user". The name "user" is reserved for the current user of the database.
    We have to use a different name for the table. We can use the table name "USERS" instead.
    Use the annotation `@Table(name = "USERS")` at the class `User` to change the table name.
    
    **tags**: #RelevantForExam

--- src/main/java/study/coco/csb/server/User.java
+++ src/main/java/study/coco/csb/server/User.java
@@ -1,14 +1,28 @@
 package study.coco.csb.server;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "USERS")
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String name;
 
   private String email;
 
+  protected User() {
+  }
+
   public User(String name, String email) {
     this.name = name;
     this.email = email;
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public String getName() {
