886bcc15dbe0e4dca8be48754563c060146495f8
Thomas Kurpick <thomas.kurpick@th-koeln.de>
2024-01-15 21:05:36 +0100

    Add UserRepository
    We add the UserRepository to gain access to the database for our User objects.
    
    **tags**: #RelevantForExam

---
+++ src/main/java/study/coco/csb/server/UserRepository.java
@@ -0,0 +1,8 @@
+package study.coco.csb.server;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
